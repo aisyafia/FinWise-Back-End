170fcdb37f64d67146b68ae5f71e3614bbb5d2fe
aisyafia <aisya.lutfia@gmail.com>
2022-12-15 11:33:59 +0100

    first commit, created models, migration and mock seeders

---
+++ migrations/20221214164531-create-user.js
@@ -0,0 +1,55 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      serviceProvider: {
+        type: Sequelize.BOOLEAN,
+      },
+      companyName: {
+        type: Sequelize.STRING,
+      },
+      companyEmail: {
+        type: Sequelize.STRING,
+      },
+      companyWebsite: {
+        type: Sequelize.STRING,
+      },
+      companyPhoneNumber: {
+        type: Sequelize.BIGINT,
+      },
+      companyLocation: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ seeders/20221215100700-some-users.js
@@ -0,0 +1,66 @@
+"use strict";
+const bcrypt = require("bcrypt");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("users", [
+      {
+        name: "Zorro",
+        email: "zorro@z.com",
+        password: bcrypt.hashSync("black", 10),
+        serviceProvider: true,
+        companyName: "BROADSTREET",
+        companyEmail: "info@broadstreet.nl",
+        companyWebsite: "https://broadstreet.nl/",
+        companyPhoneNumber: 31202624300,
+        companyLocation: "Herengracht 500, 1017 CB Amsterdam",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Yuzu",
+        email: "yuzu@y.com",
+        password: bcrypt.hashSync("citrus", 10),
+        serviceProvider: true,
+        companyName: "BDO",
+        companyEmail: "info@bdo.nl",
+        companyWebsite: "https://www.bdo.nl/en-gb/bdo-netherlands",
+        companyPhoneNumber: 31402698111,
+        companyLocation: "Krijgsman 9, 1186 DM, Amstelveen",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Whitney",
+        email: "whitney@w.com",
+        password: bcrypt.hashSync("singer", 10),
+        serviceProvider: true,
+        companyName: "Black Swan Capital",
+        companyEmail: "",
+        companyWebsite: "https://blackswancapital.eu/",
+        companyPhoneNumber: 31202091568,
+        companyLocation: "Herengracht 280, 1016 BX, Amsterdam",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Vera",
+        email: "vera@v.com",
+        password: bcrypt.hashSync("design", 10),
+        serviceProvider: true,
+        companyName: "FVB de Boer",
+        companyEmail: "",
+        companyWebsite: "https://fvbdeboer.nl/",
+        companyPhoneNumber: 31705118788,
+        companyLocation: "J. Verhulststraat 163hs, 1075 GX, Amsterdam",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
