b35ab33f902e0a6b44a6ff8011de8fc89c54cacf
aisyafia <aisya.lutfia@gmail.com>
2023-01-03 11:30:20 +0100

    feat file upload and pdf parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const corsMiddleWare = require("cors");
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
 
 const authRouter = require("./routers/auth");
 const partnerRouter = require("./routers/partners");
@@ -9,11 +11,25 @@ const { PORT } = require("./config/constants");
 const app = express();
 
 app.use(corsMiddleWare());
-
+app.use(fileUpload());
 app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/partners", partnerRouter);
+
+app.post("/upload-success", (req, res) => {
+  console.log("files:", req.files);
+  // console.log("pdf?", req);
+  if (!req.files || !req.files.pdfFile) {
+    console.log("HERE");
+    res.status(400).send(`Something went wrong, please try again`);
+    res.end();
+  }
+  pdfParse(req.files.pdfFile).then((result) => {
+    console.log("result", result.text);
+    res.send(result.text);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
